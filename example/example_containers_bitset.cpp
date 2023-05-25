29941e31c9d7577334499fd73937430fe662c1de
Tim Clephas <tim.clephas@nobleo.nl>
2023-05-25 08:16:13 +0200

    Rebase from 'debian/humble/magic_enum'

--- example/example_containers_bitset.cpp
+++ example/example_containers_bitset.cpp
@@ -1,7 +1,7 @@
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2020 - 2023 Daniil Goncharov <neargye@gmail.com>.
-// Copyright (c) 2020 - 2023 Uruha Komachin <uruhakomachin@gmail.com>.
+// Copyright (c) 2019 - 2023 Daniil Goncharov <neargye@gmail.com>.
+// Copyright (c) 2022 - 2023 Bela Schaum <schaumb@gmail.com>.
 //
 // Permission is hereby  granted, free of charge, to any  person obtaining a copy
 // of this software and associated  documentation files (the "Software"), to deal
@@ -21,25 +21,36 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#if defined(_MSC_VER)
+#  pragma warning(push)
+#  pragma warning(disable : 4244) // warning C4244: 'argument': conversion from 'const T' to 'unsigned int', possible loss of data.
+#endif
+
 #include <iostream>
 
-#include <magic_enum.hpp>
+#include <magic_enum_containers.hpp>
 
-enum class Language : int {
-    日本語 = 10,
-    한국어 = 20,
-    English = 30,
-    😃 = 40,
+enum class Color { RED = 1, GREEN = 2, BLUE = 4 };
+template <>
+struct magic_enum::customize::enum_range<Color> {
+  static constexpr bool is_flags = true;
 };
 
 int main() {
-  std::cout << magic_enum::enum_name(Language::日本語) << std::endl; // Japanese
-  std::cout << magic_enum::enum_name(Language::한국어) << std::endl; // Korean
-  std::cout << magic_enum::enum_name(Language::English) << std::endl; // English
-  std::cout << magic_enum::enum_name(Language::😃) << std::endl; // Emoji
+
+  auto color_bitset = magic_enum::containers::bitset<Color>();
+  color_bitset.set(Color::GREEN);
+  color_bitset.set(Color::BLUE);
 
   std::cout << std::boolalpha;
-  std::cout << (magic_enum::enum_cast<Language>("日本語").value() == Language::日本語) << std::endl; // true
+  std::cout << color_bitset.size() << std::endl; // 3 == magic_enum::enum_count<Color>()
+  std::cout << color_bitset.all() << std::endl; // false
+  std::cout << color_bitset.any() << std::endl; // true
+  std::cout << color_bitset.none() << std::endl; // false
+  std::cout << color_bitset.count() << std::endl; // 2
+  std::cout << color_bitset.test(Color::RED) << std::endl; // false
+  std::cout << color_bitset.test(Color::GREEN) << std::endl; // true
+  std::cout << color_bitset.test(Color::BLUE) << std::endl; // true
 
   return 0;
 }
--- example/example_containers_set.cpp
+++ example/example_containers_set.cpp
@@ -1,7 +1,7 @@
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2020 - 2023 Daniil Goncharov <neargye@gmail.com>.
-// Copyright (c) 2020 - 2023 Uruha Komachin <uruhakomachin@gmail.com>.
+// Copyright (c) 2019 - 2023 Daniil Goncharov <neargye@gmail.com>.
+// Copyright (c) 2022 - 2023 Bela Schaum <schaumb@gmail.com>.
 //
 // Permission is hereby  granted, free of charge, to any  person obtaining a copy
 // of this software and associated  documentation files (the "Software"), to deal
@@ -21,25 +21,40 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#if defined(_MSC_VER)
+#  pragma warning(push)
+#  pragma warning(disable : 4244) // warning C4244: 'argument': conversion from 'const T' to 'unsigned int', possible loss of data.
+#endif
+
+#ifdef _WIN32
+#define _ITERATOR_DEBUG_LEVEL 0
+#endif
+
 #include <iostream>
 
-#include <magic_enum.hpp>
+#include <magic_enum_containers.hpp>
 
-enum class Language : int {
-    日本語 = 10,
-    한국어 = 20,
-    English = 30,
-    😃 = 40,
+enum class Color { RED = 1, GREEN = 2, BLUE = 4 };
+template <>
+struct magic_enum::customize::enum_range<Color> {
+  static constexpr bool is_flags = true;
 };
 
 int main() {
-  std::cout << magic_enum::enum_name(Language::日本語) << std::endl; // Japanese
-  std::cout << magic_enum::enum_name(Language::한국어) << std::endl; // Korean
-  std::cout << magic_enum::enum_name(Language::English) << std::endl; // English
-  std::cout << magic_enum::enum_name(Language::😃) << std::endl; // Emoji
 
   std::cout << std::boolalpha;
-  std::cout << (magic_enum::enum_cast<Language>("日本語").value() == Language::日本語) << std::endl; // true
+  magic_enum::containers::set color_set {Color::RED, Color::GREEN, Color::BLUE};
+  std::cout << color_set.empty() << std::endl; // false
+  std::cout << color_set.size() << std::endl; // 3
+
+  color_set.clear();
+  std::cout << color_set.empty() << std::endl; // true
+  std::cout << color_set.size() << std::endl; // 0
+
+  color_set.insert(Color::GREEN);
+  color_set.insert(Color::BLUE);
+  std::cout << color_set.empty() << std::endl; // false
+  std::cout << color_set.size() << std::endl; // 2
 
   return 0;
 }
